024d6ba7a5bb3d6b242047f0b49eec94b5f3b630
sarancad01 <sarancad01@gmail.com>
2024-01-05 18:06:53 +0530

    add some files in jsprac

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ console.log(bros)
 
 //arithametic operator 
 
+
 let x=10;
 let y=67;
 let z=55
+console.log(x+y+z);
+console.log(x-y+z);
+console.log(x+y/z);
+console.log(x-y*z);
 console.log(x+y+z);
 
 
