4a9498bcf88664e2999cab59ebea7213daf41c5c
sarancad01 <sarancad01@gmail.com>
2024-01-20 18:07:08 +0530

    add mod files

--- script.js
+++ script.js
@@ -66,3 +66,41 @@ add(3,4);
 */
 
 
+
+// loop concept in javascript.
+/*
+for (let i = 0; i < 5; i++) {
+	console.log(i);
+ }
+
+
+
+let arrayExample = [];
+
+
+for (let i = -1; i < 4; i++) {
+	
+	arrayExample.push(i);
+	console.log(arrayExample);
+}
+
+
+
+var text= "welcome to welcome javascript program organized by brightskills and brightskills welcomes you once again";
+
+console.log(text.length);
+console.log(text.indexOf("you",5));
+
+*/
+
+let today= new Date();
+
+console.log(today);
+
+console.log(today.toDateString());
+
+
+console.log(today.getMonth());
+console.log(today.toLocaleString());
+console.log(today.getMilliseconds());
+console.log(today.)
